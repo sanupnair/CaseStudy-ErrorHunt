97792c6bc073a29b714710c8722a9bd1fcdccd1c
sanupnair <93447500+sanupnair@users.noreply.github.com>
2022-01-01 11:32:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require ('path'); 
 const cors = require('cors');
 const bodyParser = require ('body-parser'); // Part #1 Point 1
-const port = 5000;
+
 const nav= [
     {
         link:"/books",
